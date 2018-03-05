a87c74fbfd6ef54e9b7fbb584c4517404f52ccbe
anfegov <anfegov@gmail.com>
2018-03-05 07:14:37 -0500

    first commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,63 +1,24 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", init, false);
+function init() {
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    navigator.contacts.find(
+        [navigator.contacts.fieldType.displayName],
+        gotContacts,
+        errorHandler);
 
-        console.log('Received Event: ' + id);
+}
+
+function errorHandler(e) {
+    console.log("errorHandler: "+e);
+}
+
+function gotContacts(c) {
+    console.log("gotContacts, number of results "+c.length);
+    picDiv = document.querySelector("#pictures");
+    for(var i=0, len=c.length; i<len; i++) {
+        console.dir(c[i]);
+        if(c[i].photos && c[i].photos.length > 0) {
+            picDiv.innerHTML += "<img src='"+c[i].photos[0].value+"'>";
+        }
     }
-
-    function onSuccess(contacts) {
-    alert('Found ' + contacts.length + ' contacts.');
-};
-
-function onError(contactError) {
-    alert('onError!');
-};
-
-// find all contacts with 'Bob' in any name field
-var options = new ContactFindOptions();
-options.filter="Bob"; 
-var fields = ["displayName", "name"];
-navigator.contacts.find(fields, onSuccess, onError, options);
-};
+}
